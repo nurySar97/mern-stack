faf2c41b5a82ee7b506e8a998a2a6a4e0e82b6fe
Muhammet <nsaryyev9797@gmail.com>
2021-06-14 00:35:26 +0300

    fixed client_app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const path = require('path');
 const app = express();
 
 /* Middlewares */
@@ -8,6 +9,19 @@ app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t/', require('./routes/redirect.routes'));
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use('/', express.static(path.join(__dirname, 'client_app', 'build')));
+
+
+    app.get('*', (req, res)=>{
+
+        res.sendFile(path.resolve(__dirname, 'client_app', 'index.html'));
+
+    })
+
+}
 
 /* PORT */
 const PORT = config.get('port') || 5000;
